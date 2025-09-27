e3991c292e16ec118b9e824477b1cd65e422e0ec
Nith567 <nithinkatkam504106@gmail.com>
2025-09-27 13:04:43 +0530

    cal-scope

---
+++ contracts/calculate-scope.js
@@ -0,0 +1,6 @@
+const crypto = require('crypto');
+
+
+const seed = "crypto-nomads";
+const contractAddress = "0xaE2f2f748e6bEc92D70882cfc425e2425a0f1a92";
+console.log(`Scope for seed "${seed}" and contract "${contractAddress}": ${scope}`);
